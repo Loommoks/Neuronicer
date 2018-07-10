0a11071b1ae9a7d68ac16d047fc05c3782e21e8a
Loommoks <ga.pafnutov@gmail.com>
2018-07-10 16:23:32 +0300

    onClick listener for "loadImageButton" added

--- app/src/main/java/su/zencode/neuronicer/MainActivity.java
+++ app/src/main/java/su/zencode/neuronicer/MainActivity.java
@@ -1,13 +1,34 @@
 package su.zencode.neuronicer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int loadImage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loadImageButton = (Button) findViewById(R.id.load_image_button);
+        loadImageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loadImageIntent = new Intent(Intent.ACTION_PICK);
+
+                loadImageIntent.setType("image/*");
+
+                startActivityForResult(loadImageIntent,loadImage);
+            }
+        }
+
+        );
+
     }
 }
