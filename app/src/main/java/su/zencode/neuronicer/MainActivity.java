2e15b4f78482797ae76442c03c1297642f5a5a73
Loommoks <ga.pafnutov@gmail.com>
2018-07-10 16:38:09 +0300

    onActivityResult metod, imageView added

--- app/src/main/java/su/zencode/neuronicer/MainActivity.java
+++ app/src/main/java/su/zencode/neuronicer/MainActivity.java
@@ -1,20 +1,30 @@
 package su.zencode.neuronicer;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int loadImage = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
 
         Button loadImageButton = (Button) findViewById(R.id.load_image_button);
         loadImageButton.setOnClickListener(new OnClickListener() {
@@ -30,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
         );
 
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent imageReturnedIntent){
+        super.onActivityResult(requestCode,resultCode,imageReturnedIntent);
+
+        if (requestCode == loadImage && resultCode == RESULT_OK){
+
+            try {
+                final Uri imageUri = imageReturnedIntent.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
